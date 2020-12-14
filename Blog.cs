a913296c6a37021119b9b7b6c74b1e56976c38e4
Max-Gabriel-Susman <gabe.susman@gmail.com>
2020-12-13 20:32:00 -0700

    completed initial setup, I'm going to want to make it interactive so I can play around with it some more

---
+++ Blog.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+}
---
+++ BloggingContext.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=blogging.db");
+    }
+}
